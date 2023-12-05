e6bc04f7041355c184871dfc96fc1869bf47b814
mtakeuchi317 <138196821+mtakeuchi317@users.noreply.github.com>
2023-12-05 13:44:00 +0900

    AppServiceProvider.php初期に戻す

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Contracts\Events\Dispatcher;
-use Illuminate\Support\Facades\DB;
-use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Validator;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,27 +18,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(Dispatcher $events): void
+    public function boot(): void
     {
-        $events->listen(BuildingMenu::class, function (BuildingMenu $event) {
-            $event->menu->add('商品一覧');
-            $event->menu->add([
-                'text' => '全て',
-                'url' => 'items',
-            ]);
-
-            $categories = DB::table('items')->distinct()->pluck('category');
-
-            foreach ($categories as $category) {
-                $event->menu->add([
-                    'text' => $category,
-                    'url' => 'items/category/' . urlencode($category),
-                ]);
-            }
-        });
-
-        Validator::extend('katakana', function ($attribute, $value, $parameters, $validator) {
-            return preg_match('/[ァ-ヴー]+/u', $value);
-        });
+        //
     }
 }
