baac3e7da6b3dc975442df8a5ea502d633e8af5c
mtakeuchi317 <138196821+mtakeuchi317@users.noreply.github.com>
2023-12-13 10:06:58 +0900

    ロゴ変更戻し

--- config/adminlte.php
+++ config/adminlte.php
@@ -64,7 +64,7 @@ return [
     */
 
     'logo' => '<b>Book.jp</b>',
-    'logo_img' => 'img/Book icon.png',
+    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,10 +13,6 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    <!-- ロゴ画像の追加 -->
-    @php
-        $logo = asset(config('adminlte.logo_img'));
-    @endphp
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
